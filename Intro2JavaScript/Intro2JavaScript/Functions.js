37296d806641babf7bfa184675e5258940b7c483
javent23 <javentdesigns@gmail.com>
2017-02-18 12:26:30 -0500

    Add files via upload

---
+++ Intro2JavaScript/Intro2JavaScript/Functions.js
@@ -0,0 +1,12 @@
+//function is stated here
+function add(x,y){
+    return x+y;
+}
+//Define Variables
+var x = 10;
+var y = 30;
+
+//Return result from function and write to console
+var result = add(y,x);
+console.log('Result = '+ result);
+
